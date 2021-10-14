5e2af604d9602efdf6a91c7f66c02513bdf7921d
Blueguardian <tssa20@student.aau.dk>
2021-10-14 13:53:37 +0200

    initial ard-file update

---
+++ arduino_mega_code/arduino_mega_code.ino
@@ -0,0 +1,49 @@
+#include <Dynamixel2Arduino.h>
+#include <DynamixelShield.h>
+#include <SoftwareSerial.h>
+
+#define DXL_SERIAL   Serial
+#define DEBUG_SERIAL soft_serial
+
+const uint8_t dxl_dir_pin = 2; // DYNAMIXEL Shield DIR PIN
+
+SoftwareSerial soft_serial(7, 8); // DYNAMIXELShield UART RX/TX
+Dynamixel2Arduino dxl(DXL_SERIAL, dxl_dir_pin);
+
+
+void setup() {
+  DEBUG_SERIAL.begin(115700);
+  while(!DEBUG_SERIAL);
+
+  dxl.begin(57600);
+ //TODO: Setup pins for IO_serial
+ //IO_serial.begin(1157000);
+  
+  DYNAMIXEL::InfoFromPing_t ping_info[32];
+  while(uint8_t count = dxl.ping(0xFE, ping_info, sizeof(ping_info)/sizeof(ping_info[0])) < 5) {
+    DEBUG_SERIAL.print("Crustcrawler only found: ");
+    DEBUG_SERIAL.print(count);
+    DEBUG_SERIAL.println(" motors, retrying...");
+  }
+  /*For debugging purposes:
+   * Print motor data
+   */
+  DYNAMIXEL::InfoFromPing_t ping_motor[32];
+  if(uint8_t count_ping = dxl.ping(0xFE, ping_motor, sizeof(ping_motor)/sizeof(ping_motor[0])))
+  {
+    DEBUG_SERIAL.print("Detected motor : \n");
+    for(int i; i < count_ping; i++) {
+      DEBUG_SERIAL.print("    ");
+      DEBUG_SERIAL.print(ping_motor[i].id, DEC);
+      DEBUG_SERIAL.print(", Model: ");
+      DEBUG_SERIAL.print(ping_motor[i].model_number);
+      DEBUG_SERIAL.print(", Ver: ");
+      DEBUG_SERIAL.println(ping_motor[i].firmware_version, DEC);
+    }
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
