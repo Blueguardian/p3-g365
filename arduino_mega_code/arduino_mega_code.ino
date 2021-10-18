909e80071763144f84af1d76bf7bb139837b53f5
Blueguardian <tssa20@student.aau.dk>
2021-10-18 15:22:44 +0200

    Added initial start and stop commands

--- arduino_mega_code/arduino_mega_code.ino
+++ arduino_mega_code/arduino_mega_code.ino
@@ -51,21 +51,90 @@ void setup() {
       DEBUG_SERIAL.print(ping_motor[i].model_number);
       DEBUG_SERIAL.print(", Ver: ");
       DEBUG_SERIAL.println(ping_motor[i].firmware_version, DEC);
+      delay(1000);
     }
   }
 }
 
 void loop() {
-  uint8_t input = 0;
+  char input = 0;
   delay(10000);
   input = 1;
   if(input == 1) {
     dxl.torqueOn(2);
+    dxl.torqueOn(1);
+    dxl.torqueOn(3);
+    dxl.writeControlTableItem((uint8_t)44, (uint8_t)1, (uint32_t)10, (uint32_t)10);
+    dxl.writeControlTableItem((uint8_t)44, (uint8_t)2, (uint32_t)10, (uint32_t)10);
+    dxl.writeControlTableItem((uint8_t)44, (uint8_t)3, (uint32_t)10, (uint32_t)10);
+    dxl.writeControlTableItem((uint8_t)36, (uint8_t)1, (uint32_t)200, (uint32_t)10);
+    dxl.writeControlTableItem((uint8_t)36, (uint8_t)2, (uint32_t)200, (uint32_t)10);
+    dxl.writeControlTableItem((uint8_t)36, (uint8_t)3, (uint32_t)200, (uint32_t)10);
     dxl.setGoalPosition(2, 2048, UNIT_RAW);
-    while(dxl.getPresentPosition(2) != 2048)
+    while(!(dxl.getPresentPosition(2) < 2053 && dxl.getPresentPosition(2) > 2043))
     {
-      DEBUG_SERIAL.print("Turning...");
+      DEBUG_SERIAL.print("Motor 2 position: ");
+      DEBUG_SERIAL.println(dxl.getPresentPosition(2));
       delay(10);
-    } 
+    }
+    delay(100);
+    dxl.setGoalPosition(3, 1024, UNIT_RAW);
+    while(!(dxl.getPresentPosition(3) < 1029 && dxl.getPresentPosition(3) > 1019)) {
+      DEBUG_SERIAL.print("Motor 3 position   ");
+      DEBUG_SERIAL.println(dxl.getPresentPosition(3));
+      delay(10);
+    }
+    delay(100);
+    dxl.setGoalPosition(1, 1024, UNIT_RAW);
+    while(!(dxl.getPresentPosition(1) < 1029 && dxl.getPresentPosition(1) > 1019)) {
+      DEBUG_SERIAL.print("Motor 1 position   ");
+      DEBUG_SERIAL.println(dxl.getPresentPosition(1));
+      
+      delay(10);
+    }
+    delay(10000);
+    }
+    delay(100);
+    input = 2;
+    if(input == 2) {
+      dxl.torqueOn(2);
+      dxl.torqueOn(1);
+      dxl.torqueOn(3);
+      dxl.writeControlTableItem((uint8_t)44, (uint8_t)1, (uint32_t)10, (uint32_t)10);
+      dxl.writeControlTableItem((uint8_t)44, (uint8_t)2, (uint32_t)10, (uint32_t)10);
+      dxl.writeControlTableItem((uint8_t)44, (uint8_t)3, (uint32_t)10, (uint32_t)10);
+      dxl.writeControlTableItem((uint8_t)36, (uint8_t)1, (uint32_t)200, (uint32_t)10);
+      dxl.writeControlTableItem((uint8_t)36, (uint8_t)2, (uint32_t)200, (uint32_t)10);
+      dxl.writeControlTableItem((uint8_t)36, (uint8_t)3, (uint32_t)200, (uint32_t)10);
+      dxl.setGoalPosition(2, 2048, UNIT_RAW);
+    while(!(dxl.getPresentPosition(2) < 2053 && dxl.getPresentPosition(2) > 2043))
+    {
+      DEBUG_SERIAL.print("Motor 2 position: ");
+      DEBUG_SERIAL.println(dxl.getPresentPosition(2));
+      delay(10);
+    }
+    delay(100);
+    dxl.setGoalPosition(3, 1024, UNIT_RAW);
+    while(!(dxl.getPresentPosition(3) < 1034 && dxl.getPresentPosition(3) > 1014)) {
+      DEBUG_SERIAL.print("Motor 3 position   ");
+      DEBUG_SERIAL.println(dxl.getPresentPosition(3));
+      delay(10);
+    }
+    dxl.setGoalPosition(1, 512, UNIT_RAW);
+    while(!(dxl.getPresentPosition(1) < 517 && dxl.getPresentPosition(1) > 507)) {
+      DEBUG_SERIAL.print("Motor 1 position: ");
+      DEBUG_SERIAL.println(dxl.getPresentPosition(1));
+      delay(10);
+    }
+    dxl.setGoalPosition(2, 1174, UNIT_RAW);
+    while(!(dxl.getPresentPosition(2) < 1179 && dxl.getPresentPosition(2) > 1169)) {
+      DEBUG_SERIAL.print("Motor 2 position: ");
+      DEBUG_SERIAL.println(dxl.getPresentPosition(1));
+      delay(10);
+    }
+    delay(10000);
+      dxl.torqueOff(2);
+      dxl.torqueOff(1);
+      dxl.torqueOff(3);
     }
   }
