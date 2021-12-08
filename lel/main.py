219c3b8b7aabf82e7693a5d26576f49627c6e12b
Blueguardian <tssa20@student.aau.dk>
2021-12-08 09:36:15 +0100

    Changes to serial connection

--- lel/main.py
+++ lel/main.py
@@ -2,77 +2,90 @@ import myo
 import serial
 import sys
 
-port = serial.Serial(port='COM8', baudrate=115200)
+port = serial.Serial(port='COM8', baudrate=115200)      # Set COMPort and baudrate used for Arduino code
 
 
 class Listener(myo.DeviceListener):
-    def on_paired(self, event):
+    i = 0                                               # Used to recognize shutdown sequence
+    shutdown = [0, 0, 0]
+    j = 1                                               # Indicates currently selected motor
+
+    def on_paired(self, event):                         # Indicates that Myo is ready to go
         print("Hello, {}!".format(event.device_name))
         event.device.vibrate(myo.VibrationType.short)
 
     def on_unpaired(self, event):
-        return False  # Stop the hub
+        return False                                    # Stop the hub
 
-    def on_pose(self, event):
+    def on_pose(self, event):                           # Called when a new pose is registered
         pose = event.pose
-        if pose == myo.Pose.fist:
-            sys.stdout.write("\033[K")
-            sys.stdout.write("\033[F")
-            print("Fist")
-            if not port.isOpen():
-                port.open()
-            port.write(b'1')
-            port.close()
+        if not self.shutdown == [2, 2, 1]:              # Checks which new pose is registered unless the
+            if pose == myo.Pose.fist:                   # last three poses are: Fingersspread, Fingersspread, Fist
+                sys.stdout.write("\033[K")
+                sys.stdout.write("\033[F")
+                print("Fist")
+                port.write(b'1')                        # Sends a byte with a number representing the pose
+                self.shutdown[0] = self.shutdown[1]
+                self.shutdown[1] = self.shutdown[2]     # "Cycles" the shutdown sequence and inserts the newest pose
+                self.shutdown[2] = 1
+                print(self.shutdown)
 
-        if pose == myo.Pose.fingers_spread:
-            sys.stdout.write("\033[K")
-            sys.stdout.write("\033[F")
-            print("Fingersspread")
-            if not port.isOpen():
-                port.open()
-            port.write(b'2')
-            port.close()
+            if pose == myo.Pose.fingers_spread:
+                sys.stdout.write("\033[K")
+                sys.stdout.write("\033[F")
+                print("Fingersspread")
+                port.write(b'2')
+                self.shutdown[0] = self.shutdown[1]
+                self.shutdown[1] = self.shutdown[2]
+                self.shutdown[2] = 2
+                print(self.shutdown)
+                self.j += 1
+                if self.j == 4:
+                    self.j = 1
+                print('Motor:', self.j)
 
-        if pose == myo.Pose.wave_in:
-            sys.stdout.write("\033[K")
-            sys.stdout.write("\033[F")
-            print("Wave in")
-            if not port.isOpen():
-                port.open()
-            port.write(b'3')
-            port.close()
+            if pose == myo.Pose.wave_in:
+                sys.stdout.write("\033[K")
+                sys.stdout.write("\033[F")
+                print("Wave in")
+                port.write(b'3')
+                self.shutdown[0] = self.shutdown[1]
+                self.shutdown[1] = self.shutdown[2]
+                self.shutdown[2] = 3
+                print(self.shutdown)
 
-        if pose == myo.Pose.wave_out:
-            sys.stdout.write("\033[K")
-            sys.stdout.write("\033[F")
-            print("Wave out")
-            if not port.isOpen():
-                port.open()
-            port.write(b'4')
-            port.close()
+            if pose == myo.Pose.wave_out:
+                sys.stdout.write("\033[K")
+                sys.stdout.write("\033[F")
+                print("Wave out")
+                port.write(b'4')
+                self.shutdown[0] = self.shutdown[1]
+                self.shutdown[1] = self.shutdown[2]
+                self.shutdown[2] = 4
+                print(self.shutdown)
 
-        if pose == myo.Pose.double_tap:
-            sys.stdout.write("\033[K")
-            sys.stdout.write("\033[F")
-            print("Double tap")
-            if not port.isOpen():
-                port.open()
-            port.write(b'5')
-            port.close()
+            if pose == myo.Pose.double_tap:
+                sys.stdout.write("\033[K")
+                sys.stdout.write("\033[F")
+                print("Double tap")
+                port.write(b'5')
+                self.shutdown[0] = self.shutdown[1]
+                self.shutdown[1] = self.shutdown[2]
+                self.shutdown[2] = 5
+                print(self.shutdown)
 
-        if pose == myo.Pose.rest:
-            sys.stdout.write("\033[K")
-            sys.stdout.write("\033[F")
-            print("Rest")
-            if not port.isOpen():
-                port.open()
-            port.write(b'6')
-            port.close()
-
-        # else:
-        #     sys.stdout.write("\033[K")
-        #     sys.stdout.write("\033[F")
-        #     print("Unknown pose")
+            if pose == myo.Pose.rest:
+                sys.stdout.write("\033[K")
+                sys.stdout.write("\033[F")
+                print("------------")
+                port.write(b'9')
+        elif self.shutdown == [2, 2, 1]:
+            port.write(b'6')                            # '6' represents the shutdown sequence
+            self.shutdown = [0, 0, 0]
+            print(self.shutdown)
+            print('Shutdown')
+            self.j = 1
+            print('Motor:', self.j)
 
     def on_locked(self, event):
         print("Device is now locked")
@@ -89,11 +102,10 @@ class Listener(myo.DeviceListener):
     def on_warmup_completed(self, event):
         event.device.vibrate(myo.VibrationType.long)
 
-    def on_battery_level(self, event):
-        batt = event.battery_level
 
-
-if __name__ == '__main__':
+if __name__ == '__main__': # Main function
+    if not port.isOpen():
+        port.open()
     myo.init(sdk_path='./myo-sdk-win-0.9.0/')
     hub = myo.Hub()
     listener = Listener()
